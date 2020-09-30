d9ab88a3c6d6a3cc43572382d93b2af15095db87
Jeet Shah <44719299+jeet611@users.noreply.github.com>
2020-10-01 02:04:16 +0530

    Create SP.py

---
+++ SP.py
@@ -0,0 +1,27 @@
+# Python program for brute force method to calculate stock span values 
+
+# Fills list S[] with span values 
+def calculateSpan(price, n, S): 
+	
+	# Span value of first day is always 1 
+	S[0] = 1
+
+	# Calculate span value of remaining days by linearly 
+	# checking previous days 
+	for i in range(1, n, 1): 
+		S[i] = 1 # Initialize span value 
+
+		# Traverse left while the next element on left is 
+		# smaller than price[i] 
+		j = i - 1
+		while (j>= 0) and (price[i] >= price[j]) : 
+					S[i] += 1
+					j -= 1
+						
+
+
+# Fill the span values in list S[] 
+calculateSpan(price, n, S) 
+
+# print the calculated span values 
+printArray(S, n) 
